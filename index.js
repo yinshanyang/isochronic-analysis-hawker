464c8afd36b0cf11b19ddc0bd2b2969a9d4a9a8a
Yin Shanyang <yang@swarm.is>
2018-09-07 17:54:23 +0800

    refactor: refactor to use turf.booleanWithin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
-const turf = require('@turf/turf')
+const { featureCollection } = require('@turf/helpers')
+const booleanWithin = require('@turf/boolean-within').default
 const { Observable } = require('rxjs')
 const json = require('node-json')
 const program = require('commander')
@@ -8,42 +9,46 @@ const program = require('commander')
 program
   .version(require('./package.json').version)
   .option('-p, --point-set <path>', 'Path to source point set, `./data/point-set.geo.json`')
-  .option('-i, --input <path>', 'Path to raw responses from hawkers point set, `./data/raw`')
+  .option('-c, --contours <path>', 'Path to contours of point set, `./data/contours`')
+  .option('-i, --hawkers <path>', 'Path to feature collection of hawkers, `./data/hawkers.geo.json`')
   .option('-o, --output <path>', 'Output path, `./output/accessibility.geo.json`')
   .option('-m, --minutes <list>', 'List of minutes, `15,30,45,60`', (list) => list.split(',').map((d) => +d))
   .parse(process.argv)
 
 const POINT_SET = program.pointSet || './data/point-set.geo.json'
-const INPUT = program.input || './data/raw'
-const OUTPUT = program.output || './output/accessibility.geo.json'
+const CONTOURS = program.contours || './data/contours'
+const HAWKERS = program.hawkers || './data/hawkers.geo.json'
+const OUTPUT = program.output || './output/accessibility.json'
 const MINUTES = program.minutes || [15, 30, 45, 60]
 
 // utils
 const pointSet = json.parse(path.resolve(__dirname, POINT_SET))
+const hawkers = json.parse(path.resolve(__dirname, HAWKERS))
 
-const getCount = ({ times }) => (minutes) =>
-  times
-    .map((time) => time / 60)
-    .filter((time) => time <= minutes )
-    .length
+const getCount = (contours) => (minutes) => {
+  const contour = contours.features.find(({ properties }) => properties.time / 60 === minutes)
+  return contour
+    ? hawkers.features.filter((hawker) => booleanWithin(hawker, contour)).length
+    : 0
+}
 
 const features = pointSet.features.map(
   (feature) => {
     const { index } = feature.properties
-    const file = `${path.resolve(__dirname, INPUT)}/${index}.json`
+    const file = `${path.resolve(__dirname, CONTOURS)}/${index}.geo.json`
     const results = fs.existsSync(file)
       ? json.parse(file)
-      : {times: []}
+      : featureCollection([])
 
     const _getCount = getCount(results)
     const value = MINUTES
       .map(_getCount)
       .reduce((memo, d) => memo + d, 0)
-    const accessibility = value / (results.times.length || 1)
+    const accessibility = value / (hawkers.features.length || 1)
 
-    feature.properties = Object.assign(feature.properties, { value, accessibility })
+    feature.properties = { ...feature.properties, value, accessibility }
     return feature
   }
 )
 
-json.format(path.resolve(__dirname, OUTPUT), turf.featureCollection(features))
+json.format(path.resolve(__dirname, OUTPUT), featureCollection(features))
